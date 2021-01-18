e27a9cc6705f14ebe526d4148b45b54c477cea25
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-18 12:10:17 +0100

    Init

---
+++ vendingmachine/src/main/java/mate/rajnai/vendingmachine/App.java
@@ -0,0 +1,13 @@
+package mate.rajnai.vendingmachine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
