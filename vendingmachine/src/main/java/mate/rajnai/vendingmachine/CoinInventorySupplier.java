d3a977af8820f92849ba10bd4d9d21c7ed73a4ee
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-18 18:39:52 +0100

    Create CoinInventorySupplier class

---
+++ vendingmachine/src/main/java/mate/rajnai/vendingmachine/CoinInventorySupplier.java
@@ -0,0 +1,11 @@
+package mate.rajnai.vendingmachine;
+
+public class CoinInventorySupplier implements InventorySupplier<Coin> {
+
+	@Override
+	public void fillUp(Inventory<Coin> inventory) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
