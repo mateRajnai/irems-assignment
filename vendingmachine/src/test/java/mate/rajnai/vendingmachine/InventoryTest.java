834d9650e5b6cd92192906c2a6b08be67b07f5ee
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-18 16:53:39 +0100

    No items in inventory after instantiation

--- vendingmachine/src/test/java/mate/rajnai/vendingmachine/InventoryTest.java
+++ vendingmachine/src/test/java/mate/rajnai/vendingmachine/InventoryTest.java
@@ -2,13 +2,25 @@ package mate.rajnai.vendingmachine;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InventoryTest {
+	
+	private Inventory<Coin> inventory;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@BeforeEach
+	void init() {
+		inventory = new Inventory<Coin>();
 	}
+	
+	@Test
+	void noItemsInInventoryAfterInstantiation() {
+		List<Coin> coins = inventory.getItems();
+		assertEquals(0, coins.size());
+	}
+	
 
 }
