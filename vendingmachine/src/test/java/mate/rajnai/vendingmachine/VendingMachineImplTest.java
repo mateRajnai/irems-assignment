726690505ecdebfd11b9f49f9061ef2df57be407
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-18 14:23:14 +0100

    Create test for VendingMachineImpl

---
+++ vendingmachine/src/test/java/mate/rajnai/vendingmachine/VendingMachineImplTest.java
@@ -0,0 +1,17 @@
+package mate.rajnai.vendingmachine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+class VendingMachineImplTest {
+	
+
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
