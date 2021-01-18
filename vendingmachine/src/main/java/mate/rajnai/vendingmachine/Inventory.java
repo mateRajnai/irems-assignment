f694df57d398588f446a89474bb4d132aeb4835d
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-18 16:52:51 +0100

    Implement getItems in Inventory

--- vendingmachine/src/main/java/mate/rajnai/vendingmachine/Inventory.java
+++ vendingmachine/src/main/java/mate/rajnai/vendingmachine/Inventory.java
@@ -1,6 +1,15 @@
 package mate.rajnai.vendingmachine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inventory<T> {
+	
+	private List<T> items = new ArrayList<T>();
+
+	public List<T> getItems() {
+		return items;
+	}
 	
 	
 
