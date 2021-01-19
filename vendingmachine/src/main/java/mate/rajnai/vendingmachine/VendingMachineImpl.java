4362da7fef78b61f9a694eef140ededbcbb68c3c
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-19 20:02:11 +0100

    Convert inventory variables to final and refactor

--- vendingmachine/src/main/java/mate/rajnai/vendingmachine/VendingMachineImpl.java
+++ vendingmachine/src/main/java/mate/rajnai/vendingmachine/VendingMachineImpl.java
@@ -9,20 +9,19 @@ import mate.rajnai.vendingmachine.exception.ProductIsOutOfRunException;
 import mate.rajnai.vendingmachine.exception.VendingMachineHasNotEnoughChangeException;
 import mate.rajnai.vendingmachine.inventory.InventoriesOfVendingMachine;
 import mate.rajnai.vendingmachine.inventory.Inventory;
-import mate.rajnai.vendingmachine.inventory.InventorySupplier;
+import mate.rajnai.vendingmachine.inventory.supplier.InventorySupplier;
 
 public class VendingMachineImpl implements VendingMachine {
 	
-	private Inventory<Product> availableProducts = new Inventory<Product>();
-	private Inventory<Coin> availableCoins = new Inventory<Coin>();
-	private Inventory<Coin> insertedCoinsOfCurrentPurchase = new Inventory<Coin>();
+	private final Inventory<Product> availableProducts = new Inventory<Product>();
+	private final Inventory<Coin> availableCoins = new Inventory<Coin>();
+	private final Inventory<Coin> insertedCoinsOfCurrentPurchase = new Inventory<Coin>();
 	private List<Product> consumedProducts = new ArrayList<>();
 	
 	VendingMachineImpl(InventorySupplier<Product> productInventorySupplier, InventorySupplier<Coin> coinInventorySupplier) {
 		productInventorySupplier.fillUp(this.availableProducts);
 		coinInventorySupplier.fillUp(this.availableCoins);
 	}
-
 
 	@Override
 	public void insertCoin(Coin coin) {
@@ -47,42 +46,45 @@ public class VendingMachineImpl implements VendingMachine {
 			int changeAmount = this.getAmountOfInsertedMoneyOfCurrentPurchase() - product.getPrice();
 			List<Coin> coinsToBeAddedToAvailableCoins = this.insertedCoinsOfCurrentPurchase.clearItems();
 			this.availableCoins.addItems(coinsToBeAddedToAvailableCoins);
-			
-			List<Coin> coinsAsChange = new ArrayList<Coin>();
-			while (changeAmount > 0) {
-				if (changeAmount >= Coin.QUARTER.getValue() && this.availableCoins.hasItem(Coin.QUARTER)) {
-					this.availableCoins.removeItem(Coin.QUARTER);
-					coinsAsChange.add(Coin.QUARTER);
-					changeAmount -= Coin.QUARTER.getValue();
-					continue;
-				} else if (changeAmount >= Coin.DIME.getValue() && this.availableCoins.hasItem(Coin.DIME)) {
-					this.availableCoins.removeItem(Coin.DIME);
-					coinsAsChange.add(Coin.DIME);
-					changeAmount -= Coin.DIME.getValue();
-					continue;
-				} else if (changeAmount >= Coin.NICKEL.getValue() && this.availableCoins.hasItem(Coin.NICKEL)) {
-					this.availableCoins.removeItem(Coin.NICKEL);
-					coinsAsChange.add(Coin.NICKEL);
-					changeAmount -= Coin.NICKEL.getValue();
-					continue;
-				} else if (changeAmount >= Coin.PENNY.getValue() && this.availableCoins.hasItem(Coin.PENNY)) {
-					this.availableCoins.removeItem(Coin.PENNY);
-					coinsAsChange.add(Coin.PENNY);
-					changeAmount -= Coin.PENNY.getValue();
-					continue;
-				} else {
-					this.availableProducts.addItem(product);
-					this.takeRefund();
-					throw new VendingMachineHasNotEnoughChangeException("Not enough change. You've got back your money. Please insert the exact amount of money and select your product again.");
-				}
-				
-			}
+			List<Coin> coinsAsChange = this.collectChange(changeAmount, product);
 			this.consumedProducts.add(product);
 			return new Purchase(product, coinsAsChange);
 		}
 		else
 			throw new ProductIsOutOfRunException(product.getName() + " is out of run!");
 	}
+
+	private List<Coin> collectChange(int changeAmount, Product product) {
+		List<Coin> coinsAsChange = new ArrayList<Coin>();
+		while (changeAmount > 0) {
+			if (changeAmount >= Coin.QUARTER.getValue() && this.availableCoins.hasItem(Coin.QUARTER)) {
+				this.availableCoins.removeItem(Coin.QUARTER);
+				coinsAsChange.add(Coin.QUARTER);
+				changeAmount -= Coin.QUARTER.getValue();
+				continue;
+			} else if (changeAmount >= Coin.DIME.getValue() && this.availableCoins.hasItem(Coin.DIME)) {
+				this.availableCoins.removeItem(Coin.DIME);
+				coinsAsChange.add(Coin.DIME);
+				changeAmount -= Coin.DIME.getValue();
+				continue;
+			} else if (changeAmount >= Coin.NICKEL.getValue() && this.availableCoins.hasItem(Coin.NICKEL)) {
+				this.availableCoins.removeItem(Coin.NICKEL);
+				coinsAsChange.add(Coin.NICKEL);
+				changeAmount -= Coin.NICKEL.getValue();
+				continue;
+			} else if (changeAmount >= Coin.PENNY.getValue() && this.availableCoins.hasItem(Coin.PENNY)) {
+				this.availableCoins.removeItem(Coin.PENNY);
+				coinsAsChange.add(Coin.PENNY);
+				changeAmount -= Coin.PENNY.getValue();
+				continue;
+			} else {
+				this.availableProducts.addItem(product);
+				throw new VendingMachineHasNotEnoughChangeException("Not enough change.");
+			}	
+		}
+		return coinsAsChange;
+	}
+
 
 	@Override
 	public List<Coin> takeRefund() {
@@ -102,7 +104,5 @@ public class VendingMachineImpl implements VendingMachine {
 	@Override
 	public List<Product> getConsumedProducts() {
 		return this.consumedProducts ;
-		
 	}
-	
 }
