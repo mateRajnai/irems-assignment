60671e48694749ec579354efae2b9833db3dba00
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-19 15:54:54 +0100

    Remove empty lines

--- vendingmachine/src/main/java/mate/rajnai/vendingmachine/exception/ProductIsOutOfRunException.java
+++ vendingmachine/src/main/java/mate/rajnai/vendingmachine/exception/ProductIsOutOfRunException.java
@@ -1,9 +1,7 @@
 package mate.rajnai.vendingmachine.exception;
 
 public class ProductIsOutOfRunException extends RuntimeException {
-
 	public ProductIsOutOfRunException(String message) {
 		super(message);
 	}
-
 }
