d30df8f31305eae3b67a27404e53753f636ffe81
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-19 14:06:49 +0100

    Create ReportingImpl class

---
+++ vendingmachine/src/main/java/mate/rajnai/vendingmachine/ReportingImpl.java
@@ -0,0 +1,19 @@
+package mate.rajnai.vendingmachine;
+
+public class ReportingImpl implements Reporting {
+
+	private VendingMachine vendingMachine;
+	
+	ReportingImpl(VendingMachine vendingMachine) {
+		this.vendingMachine = vendingMachine;
+	}
+	
+	
+	@Override
+	public String reportConsumptionByProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
