b7c640d6ee3734c7fd95ad667b63a8d789fc6246
Rajnai Máté <ramate.rajnai@gmail.com>
2021-01-19 13:40:17 +0100

    Tests of InventoriesOfVendingMachine

---
+++ vendingmachine/src/test/java/mate/rajnai/vendingmachine/InventoriesOfVendingMachineTest.java
@@ -0,0 +1,33 @@
+package mate.rajnai.vendingmachine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import mate.rajnai.vendingmachine.inventory.Inventory;
+
+class InventoriesOfVendingMachineTest {
+	
+	private InventoriesOfVendingMachine inventoriesOfVendingMachine;
+	private Inventory<Product> productInventory;
+	private Inventory<Coin> coinInventory;
+	
+	@BeforeEach
+	void init() {
+		productInventory = new Inventory<>();
+		coinInventory = new Inventory<>();
+		inventoriesOfVendingMachine = new InventoriesOfVendingMachine(productInventory, coinInventory);
+	}
+
+	@Test
+	void getProductInventory() {
+		assertEquals(productInventory, inventoriesOfVendingMachine.getProductInventory());
+	}
+
+	@Test
+	void testGetCoinInventory() {
+		assertEquals(coinInventory, inventoriesOfVendingMachine.getCoinInventory());
+	}
+
+}
